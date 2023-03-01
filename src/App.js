416113a66465b308be779f6dec8f56bf479ab57b
Aegnis <pierrick.leclercq.dev@gmail.com>
2023-03-01 14:01:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DisplayDifficulty from './Components/DisplayDifficulty/DisplayDifficulty.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <p>Hello There</p>
+      <DisplayDifficulty />
+    </div>
+  )
+}
---
+++ src/Components/DisplayDifficulty/DisplayDifficulty.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import style from './displayDifficulty.module.css'
+
+export default function DisplayDifficulty (props) {
+    return (
+        <div className={style.container}>
+            <p className={style.text}>Hello world</p>
+        </div>
+    )
+}
